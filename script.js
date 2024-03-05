34bbb872f0dcadc1fe648455397bef749de5b19e
RiccardoMarroni <marroni.riccardo@gmail.com>
2024-03-05 13:41:08 +0100

    exercise complete

---
+++ script.js
@@ -0,0 +1,13 @@
+// let name = ("come ti chiami?");
+// console.log ( name );
+
+let name = prompt ("please enter your name");
+let surname = prompt ("please enter your surname");
+let FavoriteColor = prompt ("please enter yout favorite color")
+
+document.getElementById("pass").innerHTML =
+    "your password is: " + name + surname + FavoriteColor + "23";
+
+// let surname = ("qual'è il tuo cognome?")
+// console.log ( surname );
+
